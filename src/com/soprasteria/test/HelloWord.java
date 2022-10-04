5567ad58140ecec1c8eda45bf5a7c06a07b227df
MAKANI MBELOLO Belbeni Reille Kani <brk.makani@soprasteria.com>
2022-10-04 12:18:28 +0200

    modificato testo

---
+++ src/com/soprasteria/test/HelloWord.java
@@ -0,0 +1,13 @@
+package com.soprasteria.test;
+
+public class HelloWord {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Questo il primo esercizio");
+		System.out.println("Hello Word modificato");
+		
+		System.out.println("Prova del secondo commit");
+	}
+
+}
